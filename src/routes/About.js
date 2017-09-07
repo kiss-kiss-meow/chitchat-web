10d79bfacc3c6944732e2834b242eea1e32a7093
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-07 03:29:56 +0300

    Files formatting according to Prettier's config.

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Header from '../components/Header/component'
 
 const About = () => (
-    <div>
-        <Header />
-        <h1>Some info about us</h1>
-    </div>
+  <div>
+    <Header />
+    <h1>Some info about us</h1>
+  </div>
 )
 
 export default About
