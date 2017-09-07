031845870999764a8807a1688b266ba3ba4ba82d
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-07 04:14:33 +0300

    React Router lib integration. Router draft implementation.

--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './styles.css'
 
 const Header = () => (
-  <div>
-    <p>Link to Home</p>
-    <p>Link to About Us</p>
-    <p>Link to Teams</p>
+  <div className="Header">
+    <NavLink exact to="/" activeClassName="Link-selected">
+      Home
+    </NavLink>
+    <NavLink to="/about" activeClassName="Link-selected">
+      About Us
+    </NavLink>
+    <NavLink to="/sign-up" activeClassName="Link-selected">
+      Sign Up
+    </NavLink>
+    <NavLink to="/sign-in" activeClassName="Link-selected">
+      Sign In
+    </NavLink>
   </div>
 )
 
