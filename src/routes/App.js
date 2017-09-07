70b46adb1199e4c9ea9e3e0ec10fd549c89ff17e
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-07 03:29:56 +0300

    Files formatting according to Prettier's config.

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Home from './Home'
 
 const App = () => (
-    <div>
-        <Home />
-    </div>
+  <div>
+    <Home />
+  </div>
 )
 
 export default App
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Header from '../components/Header/component'
 
 const Home = () => (
-    <div>
-        <Header />
-        <h1>Hello ChitChat!</h1>
-    </div>
+  <div>
+    <Header />
+    <h1>Hello ChitChat!</h1>
+  </div>
 )
 
 export default Home
