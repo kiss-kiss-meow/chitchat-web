4836d0a8441d4ab40c1dd228f75c5fcf43969b8f
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-07 04:14:33 +0300

    React Router lib integration. Router draft implementation.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import React from 'react'
 import Home from './Home'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+import About from './About'
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/sign-up" component={SignUp} />
+      <Route exact path="/sign-in" component={SignIn} />
+      <Route exact path="/about" component={About} />
+    </div>
+  </Router>
 )
 
 export default App
